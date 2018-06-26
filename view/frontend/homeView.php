a401d298e057afe625f52104ebce1959c11a44d8
Charlotte <cha.perution@gmail.com>
2018-06-26 16:37:33 +0200

    add css

--- view/frontend/homeView.php
+++ view/frontend/homeView.php
@@ -1,21 +1,22 @@
 <?php $title = "Billet simple pour l'Alaska"; ?>
 
 <?php ob_start(); ?>
-<div>
-	<img src="../../../../public/img/head_home.jpeg" alt="bureau avec une machine à écrire et un livre" />
-	<h1>Retrouvez les derniers chapitres publiés de "Billet simple pour l'Alaska"</h1>
-</div>
+
+<div class="framePost">
 
 <?php
 while ($data = $posts->fetch()) {
 	if (!empty($data)) {
 ?>
-		<div class="news">
-		<h3>
-			<?= htmlspecialchars($data['title']); ?>
-			<em>le <?= $data['date_fr']; ?></em>
-		</h3>
-		<p>
+	
+	<div class="post">
+		<div class="headPost">
+			<h3>
+				<?= htmlspecialchars($data['title']); ?>
+				<em>le <?= $data['date_fr']; ?></em>
+			</h3>
+		</div>
+		<p class="chapters">
 		<?=
 			nl2br(htmlspecialchars($data['content']));
 		?>
@@ -23,6 +24,8 @@ while ($data = $posts->fetch()) {
 		<em><a href="index.php?action=post&amp;id=<?= $data['id']; ?>">Lire la suite ...</a></em>
 		</p>
 	</div>
+
+	
 <?php
 	} else {
 		echo "Ce billet n'existe pas.";
@@ -31,6 +34,9 @@ while ($data = $posts->fetch()) {
 
 	$posts->closeCursor();
 ?>
+
+</div>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -4,10 +4,33 @@
 		<meta charset="UTF-8">
 		<title><?= $title ?></title>
 	    <link href="public/css/style.css" rel="stylesheet" /> 
+	    <link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC|Pacifico|Roboto|Slabo+27px" rel="stylesheet">
 	    <!-- ajouter meta -->
 	</head>
 
 	<body>
+		<header>
+			<nav id="menu">
+				<ul>
+					<li><a>Accueil</a></li>
+					<li><a>A propos</a></li>
+					<li><a>Connexion/ Inscription</a></li>
+				</ul>
+			</nav>
+
+			<div id="header_Img">
+					<img src="public/img/headhome.jpg" alt="bureau avec une machine à écrire et un livre" />
+					<div id="header_content">
+						<h1>Billet simple pour l'Alaska</h1>
+						<p id="authorName">Jean Forteroche</p>
+					</div>
+			</div>
+		</header>
 		<?= $content ?>
+
+		<footer>
+			<p>Mentions Légales</p>
+			<p>Images libres de droit</p>
+		</footer>
 	</body>
 </html>
