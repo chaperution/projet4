56cd49d1d2f5dfc784f282246194f86c81c35f74
Charlotte <cha.perution@gmail.com>
2018-06-29 16:36:59 +0200

    add functionnality addMember in controller

--- controller/frontend.php
+++ controller/frontend.php
@@ -3,17 +3,16 @@
 
 require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
+require_once('model/SubscribeManager.php');
 
-function listPosts()
-{
+function listPosts() {
     $postManager = new \projet4\Blog\Model\PostManager(); 
     $posts = $postManager->getPosts(); 
 
     require('view/frontend/homeView.php');
 }
 
-function post()
-{
+function post() {
     $postManager = new \projet4\Blog\Model\PostManager();
     $commentManager = new \projet4\Blog\Model\CommentManager();
 
@@ -23,20 +22,31 @@ function post()
     require('view/frontend/postView.php');
 }
 
-function addComment($postId, $author, $comment)
-{
+function addComment($postId, $author, $comment) {
     $commentManager = new \projet4\Blog\Model\CommentManager();
 
     $affectedLines = $commentManager->postComment($postId, $author, $comment);
 
     if ($affectedLines === false) {
-        throw new Exception('Impossible d\'ajouter le commentaire !');
+        throw new Exception("Impossible d'ajouter le commentaire !");
     }
     else {
         header('Location: index.php?action=post&id=' . $postId);
     }
 }
 
+function addMember($pseudo, $pass, $mail) {
+	$subscribeManager = new \projet4\Blog\Model\SubscribeManager();
 
-// Hachage du mot de passe
-//$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT); + validité des autres infos
+	$newMember = $subscribeManager->createMember($pseudo, $pass, $mail);
+
+	// Hachage du mot de passe
+	$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+	// vérification validité du pseudo et du mail p/r à la bdd
+	if (strtolower($_POST['pseudo']) == strtolower($pseudo['pseudo']) {
+		throw new Exception("Ce pseudo est déjà utilisé !");
+	} elseif (strtolower($_POST['mail'])) == strtolower($mail['mail']) {
+		throw new Exception("Cette adresse mail est déjà utilisée !");
+	}
+}
